381eec9b006e9816dbcb676d4f7a77b86b0f7955
VoidKeishi <phananha4@gmail.com>
2024-05-26 16:04:07 +0700

    add 2 files

---
+++ Easy/217.contain_duplicate.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        
+    }
+};
+int main(){
+    Solution solution;
+}
